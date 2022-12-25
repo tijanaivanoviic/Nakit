321d1261b8b00f57de4b88c3895ca7e4933f07cc
tijanaivanoviic <tijanaivanovic616@gmail.com>
2022-12-25 17:32:58 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+/* 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+}); */
+
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('materijali', \App\Http\Controllers\MaterijalController::class);
+    Route::resource('vrste', \App\Http\Controllers\VrstaController::class);
+    Route::resource('nakit', \App\Http\Controllers\NakitController::class);
+});
